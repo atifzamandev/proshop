110b352f8e1be309cc0cfd5198df522fedce9fea
Atif Zaman <atif.zaman@rapunzel.com>
2023-09-03 20:31:13 +0200

    S

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
+import products from './data/products.js'
+const port = process.env.PORT || 5050;
+
+const app = express()
+
+app.get('/', (request, response)=>{
+    response.send('Api is running...')
+})
+
+app.get('/api/products' , (request, response)=>{
+    response.json(products)
+})
+
+app.get('/api/products/:id', (request, response) => {
+    const product = products.find((p)=> p._id === request.params.id)
+    response.json(product)
+})
+
+
+app.listen(port, ()=>console.log(`Server is running on port ${port}`))
--- frontend/src/Pages/ProductPage.jsx
+++ frontend/src/Pages/ProductPage.jsx
@@ -1,16 +1,30 @@
-import { useParams } from 'react-router-dom';
-import { Link } from 'react-router-dom';
-import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
-import Rating from '../components/Rating';
-import products from '../products';
+import { useParams } from "react-router-dom"
+import { Link } from "react-router-dom"
+import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap"
+import Rating from "../components/Rating"
+import { useEffect, useState } from "react"
+import axios from "axios"
 
 const ProductPage = () => {
-  const { id: productId } = useParams();
-  const product = products.find((p) => p._id === productId);
+  const [product, setProduct] = useState({})
+
+  const { id: productId } = useParams()
+
+  
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${productId}`)
+      setProduct(data)
+      console.log('This is product', data)
+    }
+    fetchProduct()
+  }, [productId])
+
+  //const product = products.find((p) => p._id === productId);
 
   return (
     <>
-      <Link className='btn btn-light my-3' to='/'>
+      <Link className="btn btn-light my-3" to="/">
         Go Back
       </Link>
       <Row>
@@ -18,7 +32,7 @@ const ProductPage = () => {
           <Image src={product.image} alt={product.name} fluid />
         </Col>
         <Col md={4}>
-          <ListGroup variant='flush'>
+          <ListGroup variant="flush">
             <ListGroup.Item>
               <h3>{product.name}</h3>
             </ListGroup.Item>
@@ -28,17 +42,13 @@ const ProductPage = () => {
                 text={`${product.numReviews} raviews`}
               />
             </ListGroup.Item>
-            <ListGroup.Item>
-            Price: ${product.price}
-            </ListGroup.Item>
-            <ListGroup.Item>
-                Description: {product.description}
-            </ListGroup.Item>
+            <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+            <ListGroup.Item>Description: {product.description}</ListGroup.Item>
           </ListGroup>
         </Col>
         <Col md={3}>
           <Card>
-            <ListGroup variant='flush'>
+            <ListGroup variant="flush">
               <ListGroup.Item>
                 <Row>
                   <Col>Price:</Col>
@@ -47,15 +57,21 @@ const ProductPage = () => {
               </ListGroup.Item>
               <ListGroup.Item>
                 <Row>
-                  <Col>Status:</Col>? 
-                  <Col><strong>
-                  ${product.countInStock > 0 ? 'In Stock' : 'Out Of Stock' }
-                    </strong></Col>
+                  <Col>Status:</Col>?
+                  <Col>
+                    <strong>
+                      ${product.countInStock > 0 ? "In Stock" : "Out Of Stock"}
+                    </strong>
+                  </Col>
                 </Row>
               </ListGroup.Item>
               <ListGroup.Item>
-                <Button className='btn-block' type='button' disabled= {product.countInStock === 0}>
-                Add To Cart
+                <Button
+                  className="btn-block"
+                  type="button"
+                  disabled={product.countInStock === 0}
+                >
+                  Add To Cart
                 </Button>
               </ListGroup.Item>
             </ListGroup>
@@ -63,7 +79,7 @@ const ProductPage = () => {
         </Col>
       </Row>
     </>
-  );
-};
+  )
+}
 
-export default ProductPage;
+export default ProductPage
