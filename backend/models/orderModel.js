33cdd5e3381886ada52ac56813b97e21be8a889b
Atif Zaman <atif.zaman@rapunzel.com>
2023-09-11 10:38:24 +0200

    MongoDB scheme and model setup

---
+++ backend/models/orderModel.js
@@ -0,0 +1,83 @@
+import mongoose from "mongoose"
+
+const orderSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    ordreItems: [
+      {
+        name: { type: String, required: true },
+        qty: { type: String, required: true },
+        image: { type: String, required: true },
+        price: { type: String, required: true },
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          required: true,
+          ref: "Product",
+        },
+      },
+    ],
+    shippingAddress: {
+      address: { type: String, required: true },
+      city: { type: String, required: true },
+      postalCode: { type: String, required: true },
+      country: { type: String, required: true },
+    },
+    paymentMethod: {
+      type: String,
+      required: true,
+    },
+    paymentResult: {
+      id: {},
+      status: {},
+      udpate_time: {},
+      email_address: {},
+    },
+    itemsPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
+    },
+    taxPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
+    },
+    shippingPrice: {
+      type: Number,
+      requried: true,
+      default: 0.0,
+    },
+    totalPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
+    },
+    isPaid: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    paidAt: {
+      type: Date,
+    },
+    isDelivered: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    deliveredAt: {
+      type: Date,
+    },
+  },
+  {
+    timesStamps: true,
+  }
+)
+
+const Order = mongoose.model("Order", orderSchema)
+
+export default Order
